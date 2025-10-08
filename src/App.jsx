cc65b06d62ea178d6b1ed649a9709124f3e33153
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-10-07 19:16:43 -0700

    add conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,29 @@ function App() {
     setScore(0)
   }
 
+  let winElement;
+  if(score>100) {
+    winElement = <h1> You Win!</h1>
+  } else {
+    winElement = <h1> Keep trying </h1>
+  }
 
   return (
     <>
       <h1 className="score-board" style={headerStyle}>Basketball Score {score}</h1>
+      {winElement}
       <div style={{ display: "flex", justifyContent: "space-around" }}>
         <button type="button" className="btn btn-primary"  onClick={freeThrow}>Free Throw</button>
         <button type="button" className="btn btn-info"     onClick={twoPointShot}>Two Point Shot</button>
         <button type="button" className="btn btn-warning"  onClick={threePointShot}>Three Pointer</button>
         <button type="button" className="btn btn-danger"   onClick={resetScore}>Reset Score</button>
+      </div>
+      <div style={{fontSize: score, color: score<100 ? "black" : "red"}}>
+        Score: {score}
+        <br/>
+        {
+          score<100 ? <h2>Keep Working</h2> : <h2>Congratulations</h2>
+        }
       </div>
       <div style={footerStyle}>
         Score: {score}
