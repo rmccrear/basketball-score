9a3d11ede9df5649f839016cc2e112149dc18575
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-10-07 18:27:52 -0700

    add style in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,55 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0)
+
+  // 3 pointer
+  function threePointShot() {
+    setScore(score+3)
+  }
+  // 2 point shot
+  function twoPointShot() {
+    setScore(score+2)
+  }
+
+  // free throw
+  function freeThrow() {
+    setScore(score+1)
+  }
+
+  function resetScore() {
+    // score = 0;
+    // updateScreen();
+    setScore(0)
+  }
+
 
   return (
     <>
-      <h1>Basketball Score</h1>
+      <h1 className="score-board" style={headerStyle}>Basketball Score {score}</h1>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <button type="button" className="btn btn-primary"  onClick={freeThrow}>Free Throw</button>
+        <button type="button" className="btn btn-info"     onClick={twoPointShot}>Two Point Shot</button>
+        <button type="button" className="btn btn-warning"  onClick={threePointShot}>Three Pointer</button>
+        <button type="button" className="btn btn-danger"   onClick={resetScore}>Reset Score</button>
+      </div>
+      <div style={footerStyle}>
+        Score: {score}
+      </div>
     </>
   )
+}
+
+const headerStyle = {
+  backgroundColor: "blue",
+  color: "white",
+  padding: "1em"
+}
+
+const footerStyle = {
+  textAlign: "center",
+  fontSize: "small",
+  padding: "3em"
 }
 
 export default App
