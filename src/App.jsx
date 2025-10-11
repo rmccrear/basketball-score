57c3f0f3df318adc4cca2dbe6e521536b3b0df19
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-10-10 20:24:08 -0700

    practice arrays and loops with dice Lesson 7 Level 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import Dice from './components/Dice'
 
 function App() {
   const [score, setScore] = useState(0)
 
   // 3 pointer
   function threePointShot() {
-    setScore(score+3)
+    setScore(score + 3)
   }
   // 2 point shot
   function twoPointShot() {
-    setScore(score+2)
+    setScore(score + 2)
   }
 
   // free throw
   function freeThrow() {
-    setScore(score+1)
+    setScore(score + 1)
   }
 
   function resetScore() {
@@ -25,7 +26,7 @@ function App() {
   }
 
   let winElement;
-  if(score>100) {
+  if (score > 100) {
     winElement = <h1> You Win!</h1>
   } else {
     winElement = <h1> Keep trying </h1>
@@ -33,23 +34,29 @@ function App() {
 
   return (
     <>
-      <h1 className="score-board" style={headerStyle}>Basketball Score {score}</h1>
-      {winElement}
-      <div style={{ display: "flex", justifyContent: "space-around" }}>
-        <button type="button" className="btn btn-primary"  onClick={freeThrow}>Free Throw</button>
-        <button type="button" className="btn btn-info"     onClick={twoPointShot}>Two Point Shot</button>
-        <button type="button" className="btn btn-warning"  onClick={threePointShot}>Three Pointer</button>
-        <button type="button" className="btn btn-danger"   onClick={resetScore}>Reset Score</button>
-      </div>
-      <div style={{fontSize: score, color: score<100 ? "black" : "red"}}>
-        Score: {score}
-        <br/>
-        {
-          score<100 ? <h2>Keep Working</h2> : <h2>Congratulations</h2>
-        }
-      </div>
-      <div style={footerStyle}>
-        Score: {score}
+      <div className="container">
+        <h1 className="score-board" style={headerStyle}>Basketball Score {score}</h1>
+        {winElement}
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <button type="button" className="btn btn-primary" onClick={freeThrow}>Free Throw</button>
+          <button type="button" className="btn btn-info" onClick={twoPointShot}>Two Point Shot</button>
+          <button type="button" className="btn btn-warning" onClick={threePointShot}>Three Pointer</button>
+          <button type="button" className="btn btn-danger" onClick={resetScore}>Reset Score</button>
+        </div>
+        <div style={{ fontSize: score, color: score < 100 ? "black" : "red" }}>
+          Score: {score}
+          <br />
+          {
+            score < 100 ? <h2>Keep Working</h2> : <h2>Congratulations</h2>
+          }
+        </div>
+        <div style={footerStyle}>
+          Score: {score}
+        </div>
+
+        <br />
+        <h2>Care to play dice?</h2>
+        <Dice />
       </div>
     </>
   )
---
+++ src/components/Dice.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react"
+
+function randomNumber(a, b) {
+    return Math.floor(Math.random() * (b - a)) + a;
+}
+
+function DiceFace({ faceValue }) {
+    return (<>
+        <div style={{ width: "100px", height: "100px", border: "solid black", fontSize: "70px" }}>
+            {faceValue}
+        </div>
+    </>)
+}
+
+export default function Dice() {
+    const [odds, setOdds] = useState(0);
+    // var odds = 0
+    const [diceRollsList, setDiceRollsList] = useState([])
+    // var diceRollsList = []
+
+    function play() {
+        let tempRollsList = []
+        let tempOdds = 0;
+        setOdds(0);
+
+        for (let i = 0; i < 10; i++) {
+            let roll = randomNumber(1, 6)
+            // appendItem(tempRollList, roll)
+            tempRollsList.push(roll) // append item (A.K.A. push)
+
+            console.log("roll: " + roll)
+            if ((roll == 1) || (roll == 3) || (roll == 5)) {
+                tempOdds++
+            }
+        }
+        console.log(tempRollsList)
+        setDiceRollsList(tempRollsList)
+        setOdds(tempOdds)
+    }
+
+    let diceDisplay = []
+    for (let i = 0; i < diceRollsList.length; i++) {
+        let borderValue = "solid black"
+        if(diceRollsList[i] === 1 || (diceRollsList[i] === 3) || (diceRollsList[i]===5)) {
+            borderValue = "solid red"
+        } 
+        let diceFace = (
+            <div style={{ margin: "2px", width: "100px", height: "100px", border: borderValue, fontSize: "70px" }}>
+                {diceRollsList[i]}
+            </div>
+        )
+        diceDisplay.push(diceFace)
+    }
+
+
+    return (<>
+        <h3>Odds: {odds}</h3>
+        <button className="btn btn-primary" onClick={play}>Play</button>
+        <div style={{ display: "flex" }}>
+            {diceDisplay}
+        </div>
+    </>)
+}
+
+
+
